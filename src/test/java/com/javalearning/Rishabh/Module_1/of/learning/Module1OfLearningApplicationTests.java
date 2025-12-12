935c343c4fd54d9c0057c5537114ca0315ec4694
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-13 02:17:00 +0530

    Initial commit

---
+++ src/test/java/com/javalearning/Rishabh/Module_1/of/learning/Module1OfLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.Rishabh.Module_1.of.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1OfLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
